02b577b39c225f2cd57ca30fd0ec9566e47f5283
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-11-25 00:09:22 +0500

    e commerce website

---
+++ functions.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+function pr($str) {
+
+    echo '<pre>';
+    print_r($arr);
+}
+
+// when we have to check the status of array then this function called
+
+function prx($arr) {
+
+    echo '<pre>';
+    print_r($arr);
+}
+
+function get_safe_value($conn,$str) {
+
+    // if we have to update some thing so we can update in this function
+    if($str!=''){
+        return mysqli_real_escape_string($conn,$str);
+        }
+}
+?>
